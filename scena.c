97094427c0ca83cf241a6bd713ac64b84e670521
jovica <jovica98@yahoo.com>
2020-01-01 13:45:07 +0100

    prepravljena scena i nacrtana zicana kocka kao test

--- scena.c
+++ scena.c
@@ -4,27 +4,52 @@
 
 static void onDisplay(void);
 static void onKeyboard(unsigned char key,int x,int y);
+static void onReshape(int width,int height);
 //static void onTimer(void);
+
+static int _width,_height;
 
 int main(int argc,char** argv){
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1024,1024);
+	glutInitWindowSize(512,512);
 	glutInitWindowPosition(100,100);
 	
 	glutCreateWindow("Bezi i pokupi");
 	
+	
 	glutKeyboardFunc(onKeyboard);
+	glutReshapeFunc(onReshape);
 	glutDisplayFunc(onDisplay);
+	glClearColor(0,1,0,0);
 	
-	glutSwapBuffers();
-	
+	glEnable(GL_DEPTH_TEST);
 	glutMainLoop();
 	return 0;
 }
 
 static void onDisplay(void){
-	glClearColor(0,0,0,0);
+	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	//glViewport(0,0,width,height);
+	glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(
+            60,
+            _width/(float)_height,
+            1, 5);
+            glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(
+            1, 2, 3,
+            0, 0, 0,
+            0, 1, 0
+        );
+	
+	glColor3f(1,1,1);
+	glutWireCube(1);
+	glutSwapBuffers();
 }
 
 static void onKeyboard(unsigned char key,int x,int y){
@@ -39,3 +64,9 @@ static void onKeyboard(unsigned char key,int x,int y){
 	}
 }
 	
+static void onReshape(int width,int height){
+	_width=width;
+	_height=height;
+    //glutPostRedisplay();
+}
+
